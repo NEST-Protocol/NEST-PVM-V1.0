acabe828a4b1912b42014c0eb3e439f565b638d1
chenf <baiynui@hotmail.com>
2022-03-04 15:09:24 +0800

    Use calculated standard normal distribution value

--- test/7.HedgeOptions-test2-hbtc.js
+++ test/7.HedgeOptions-test2-hbtc.js
@@ -199,8 +199,7 @@ describe('HedgeOptions', function() {
                 let cal = 1000 * USDT_BASE / vc;
                 console.log('cal: ' + cal);
 
-                // TODO: check
-                //expect(Math.abs(parseFloat(toDecimal(await hedgeOptions.balanceOf(fot.index, owner.address))) - cal)).to.lt(0.0001);
+                expect(Math.abs(parseFloat(toDecimal(await hedgeOptions.balanceOf(fot.index, owner.address))) - cal)).to.lt(0.0001);
                 
                 // exercise
                 let fotBalance = await hedgeOptions.balanceOf(fot.index, owner.address);
