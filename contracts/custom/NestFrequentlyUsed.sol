5de187eb7abe37af63f9609052348d55bd9943c1
chenf <baiynui@hotmail.com>
2022-07-15 15:44:01 +0800

    Determine constant value

--- contracts/custom/NestFrequentlyUsed.sol
+++ contracts/custom/NestFrequentlyUsed.sol
@@ -4,40 +4,61 @@ pragma solidity ^0.8.6;
 
 import "../NestBase.sol";
 
-// /// @dev This contract include frequently used data
-// contract NestFrequentlyUsed is NestBase {
+/// @dev This contract include frequently used data
+contract NestFrequentlyUsed is NestBase {
 
-//     // Address of nest token
-//     address NEST_TOKEN_ADDRESS;
+    // ETH:
+    // Address of nest token
+    address constant NEST_TOKEN_ADDRESS = 0x04abEdA201850aC0124161F037Efd70c74ddC74C;
+    // Address of NestOpenPrice contract
+    address constant NEST_OPEN_PRICE = 0xE544cF993C7d477C7ef8E91D28aCA250D135aa03;
+    // Address of nest vault
+    address constant NEST_VAULT_ADDRESS;
 
-//     // Address of NestOpenPrice contract
-//     address constant NEST_OPEN_PRICE = 0x09CE0e021195BA2c1CDE62A8B187abf810951540;
+    // BSC:
+    // Address of nest token
+    address constant NEST_TOKEN_ADDRESS = 0x98f8669F6481EbB341B522fCD3663f79A3d1A6A7;
+    // Address of NestOpenPrice contract
+    address constant NEST_OPEN_PRICE = 0x09CE0e021195BA2c1CDE62A8B187abf810951540;
+    // Address of nest vault
+    address constant NEST_VAULT_ADDRESS;
 
-//     // Address of nest vault
-//     address NEST_VAULT_ADDRESS;
+    // Polygon:
+    // Address of nest token
+    address constant NEST_TOKEN_ADDRESS = 0x98f8669F6481EbB341B522fCD3663f79A3d1A6A7;
+    // Address of NestOpenPrice contract
+    address constant NEST_OPEN_PRICE = 0x09CE0e021195BA2c1CDE62A8B187abf810951540;
+    // Address of nest vault
+    address constant NEST_VAULT_ADDRESS;
 
-//     // USDT base
-//     uint constant USDT_BASE = 1 ether;
-// }
+    // KCC:
+    // Address of nest token
+    address constant NEST_TOKEN_ADDRESS = 0x98f8669F6481EbB341B522fCD3663f79A3d1A6A7;
+    // Address of NestOpenPrice contract
+    address constant NEST_OPEN_PRICE = 0x7DBe94A4D6530F411A1E7337c7eb84185c4396e6;
+    // Address of nest vault
+    address constant NEST_VAULT_ADDRESS;
+
+    // USDT base
+    uint constant USDT_BASE = 1 ether;
+}
 
 import "../interfaces/INestGovernance.sol";
 
+// TODO:
 /// @dev This contract include frequently used data
 contract NestFrequentlyUsed is NestBase {
 
     // Address of nest token
     address NEST_TOKEN_ADDRESS;
-
     // Address of NestOpenPrice contract
     address NEST_OPEN_PRICE;
-    
     // Address of nest vault
     address NEST_VAULT_ADDRESS;
 
     // USDT base
     uint constant USDT_BASE = 1 ether;
 
-    // TODO:
     /// @dev Rewritten in the implementation contract, for load other contract addresses. Call 
     ///      super.update(newGovernance) when overriding, and override method without onlyGovernance
     /// @param newGovernance INestGovernance implementation contract address
