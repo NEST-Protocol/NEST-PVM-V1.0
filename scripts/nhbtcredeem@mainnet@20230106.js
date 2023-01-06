f4716e90588f6c8299c135b461ffe99d29494f18
chenf <baiynui@hotmail.com>
2023-01-06 16:16:18 +0800

    Deploy NestRedeem

--- scripts/nhbtcredeem@mainnet@20230106.js
+++ scripts/nhbtcredeem@mainnet@20230106.js
@@ -19,11 +19,12 @@ exports.deploy = async function() {
     // nest: 0x04abEdA201850aC0124161F037Efd70c74ddC74C
     // nestGovernance: 0xA2eFe217eD1E56C743aeEe1257914104Cf523cf5
     // ntokenMining: 0xC2058Dd4D55Ae1F3e1b0744Bdb69386c9fD902CA
+    // nestRedeem: 0xAF22d05095d09cb6CB4f18cb7aeFd94Cb39EB113
     // proxyAdmin: 0xCA52f25f37d213CeD3dDE81a539e64464dEa8f3C
 
     //const nhbtc = await TestERC20.deploy('nHBTC', 'nHBTC', 18);
     const nhbtc = await TestERC20.attach('0x1F832091fAf289Ed4f50FE7418cFbD2611225d46');
-    console.log('nhbtc: ' + hbtc.address);
+    console.log('nhbtc: ' + nhbtc.address);
 
     //const nest = await TestERC20.deploy('NEST', 'NEST', 18);
     const nest = await TestERC20.attach('0x04abEdA201850aC0124161F037Efd70c74ddC74C');
@@ -33,11 +34,11 @@ exports.deploy = async function() {
     const nestGovernance = await NestGovernance.attach('0xA2eFe217eD1E56C743aeEe1257914104Cf523cf5');
     console.log('nestGovernance: ' + nestGovernance.address);
 
-    const nestRedeem = await NestRedeem.deploy(nhbtc.address, nest.address, 500000000000000000n);
-    //const nestRedeem = await NestRedeem.attach('0x0000000000000000000000000000000000000000');
+    //const nestRedeem = await NestRedeem.deploy(nhbtc.address, nest.address, 500000000000000000n, { nonce: 74 });
+    const nestRedeem = await NestRedeem.attach('0xAF22d05095d09cb6CB4f18cb7aeFd94Cb39EB113');
     console.log('nestRedeem: ' + nestRedeem.address);
 
-    await nestRedeem.initialize(nestGovernance.address);
+    //await nestRedeem.initialize(nestGovernance.address, { nonce: 75 });
 
     console.log('---------- OK ----------');
     
@@ -47,7 +48,7 @@ exports.deploy = async function() {
 
     const contracts = {
         eth: eth,
-        hbtc: hbtc,
+        nhbtc: nhbtc,
         nest: nest,
 
         nestGovernance: nestGovernance,
