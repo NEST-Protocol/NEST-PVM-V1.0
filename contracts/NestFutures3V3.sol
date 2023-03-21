be1fd6d8d2d76f31e8ad0081bedd3e5299cc02a5
chenf <baiynui@hotmail.com>
2023-03-21 13:53:51 +0800

    Update liquidate line

--- contracts/NestFutures3V3.sol
+++ contracts/NestFutures3V3.sol
@@ -330,9 +330,10 @@ contract NestFutures3V3 is NestFrequentlyUsed, INestFutures3 {
                 // 5. Liquidate logic
                 // lever is great than 1, and balance less than a regular value, can be liquidated
                 // the regular value is: Max(M0 * L * St / S0 * c, a) | expired
-                // the regular value is: Max(M0 * L * St / S0 * c + a, M0 * L * 0.5%)
+                // the regular value is: Max(M0 * L * St / S0 * c + a, M0 * L * 0.5%) | expired
+                // the regular value is: Max(M0 * L * St / S0 * c + a, M0 * L * 1%)
                 unchecked {
-                    if (value < balance / 200 || value < fee + CommonLib.MIN_FUTURE_VALUE) {
+                    if (value < balance / 100 || value < fee + CommonLib.MIN_FUTURE_VALUE) {
                         // Clear all data of order, use this code next time
                         assembly {
                             mstore(0, _orders.slot)
