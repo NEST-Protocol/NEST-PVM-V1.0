286aee233849fd82d9d29f298e545bc0044d0ef7
chenf <baiynui@hotmail.com>
2023-09-26 15:18:50 +0800

    release

--- contracts/ArithFi.sol
+++ contracts/ArithFi.sol
@@ -4,11 +4,15 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-/// @dev This is the only valid NEST token issued by the NEST project team on BSC
+/// @dev This is the only valid NEST token issued by the NEST project team on ETH&BSC
 contract ArithFi is ERC20 {
     
     constructor() ERC20("ArithFi", "ATF") {
+        // On ETH
         _mint(msg.sender, 1000000000 ether);
+
+        // On BSC
+        //_mint(msg.sender, 300000000 ether);
     }
 
 }
